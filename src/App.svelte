df8b9032b84e47ecce745ca16ba4bcf6d93f0e01
Marwan Aziz <aziz.marwan@gmail.com>
2020-02-01 14:28:38 -0500

    add login logout

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,20 @@
 <script>
 	let name = 'world';
+	let loggedIn = false;
+	function login() {
+		loggedIn = true;
+	}
+	const logout = () => {
+		loggedIn = false
+	};
 </script>
 
-<h1>Hello {name}!</h1>
+{#if loggedIn}
+	<h1>You are logged in</h1>
+	<button on:click={logout}>Log Out</button>
+	{:else}
+	<h1>You are logged out</h1>
+	<button on:click={login}>Log In</button>
+{/if}
+
+
